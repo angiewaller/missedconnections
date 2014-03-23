5fa39939d5478f587ff4d6a36a2d7a15b64c2e54
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2014-03-23 13:58:32 -0400

    preliminary tagging system

--- analyzeText.py
+++ analyzeText.py
@@ -22,14 +22,51 @@ def splitSentences():
 				sentences.remove(sentence)
 
 			if len(sentence) > 0:
-				result = analyzeWords(sentence)
+				result = sortPhrases(sentence)
+
+				if len(result) > 0:
+					print result
+
+					# if result[0] == "intro":
+					# 	print sentence
 
 		#print sentences
 
-def analyzeWords(sentence):
+def sortPhrases(sentence):
 
-	tokens = nltk.Text(nltk.word_tokenize(sentence))
-	key = tokens.findall(r"<you><were><.*>{3,}")
+	tags = []
+
+	intro = ["You were", "I was", "I pulled up", "I swear", "I know", "I saw", "You saw"]
+	interaction = ["nodded", "shared", "called", "talk", "talking", "we met", "locked eyes", "stared", "looked", "looking for", "smiled"]
+	description = ["looked like", "saw", "were reading", "was reading", "ogle", "ogled", "ogling", "got in"]
+	afterthought = ["I believe", "I feel", "felt like", "think about", "I think", "I thought", "forgot", "wanted", "didn't want"]
+
+	for phrase in intro:
+		key = re.match(phrase, sentence)
+		if key != None:
+			tags.append("intro")
+			break
+
+	for phrase in interaction:
+		key = re.search(phrase, sentence)
+		if key != None:
+			tags.append("interaction")
+			break
+
+	for phrase in description:
+		key = re.search(phrase, sentence)
+		if key != None:
+			tags.append("description")
+			break
+
+	for phrase in afterthought:
+		key = re.search(phrase, sentence)
+		if key != None:
+			tags.append("afterthought")
+			break
+
+	return tags
+
 
 splitSentences()
 
