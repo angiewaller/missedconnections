47f438d1d2d231ff3adf2e03525e7d67a0cfb0a3
Roopa Vasudevan <roopa.vasudevan@gmail.com>
2014-03-31 21:10:06 -0400

    more text, printing to file

--- createNovel.py
+++ createNovel.py
@@ -1,6 +1,7 @@
 from sys import argv
 import sqlite3
 from random import randrange
+import datetime
 
 #create an optional argument for a city-specific novel
 script, city = argv
@@ -11,14 +12,72 @@ descriptions = []
 interactions = []
 afterthoughts = []
 
+#initialize blank list, this is where we are storing the novel
+novel = []
+
+def selectSentence(type):
+
+	seed = randrange(len(type))
+	return seed
+
 def printNovel():
 
-	#pick a sentence at random from the list and print it
-	introSelect = randrange(len(intros))
-	descSelect = randrange(len(descriptions))
-	interSelect = randrange(len(interactions))
-	afterSelect = randrange(len(afterthoughts))
-	print intros[introSelect] + ".  " + descriptions[descSelect] + ".  " + interactions[interSelect] + ".  " + afterthoughts[afterSelect] + "."
+	#generates a unique file name with the city and the date/time
+	pubdate = datetime.datetime.now()
+	filename = city + "-" + pubdate.strftime("%B%d%y-%I%M%p") + ".txt"
+
+	#pick a first sentence at random from the list, append it to the novel lis
+	firstSentence = selectSentence(intros)
+	novel.append(intros[firstSentence])
+	currentSentence = 0
+	count = len(intros[firstSentence])
+
+	#while the total character count is less than 5000...
+	while count < 5000:
+
+		#picking new sentence type based on last sentence type
+		#intro sentence is only used at the beginning.  the other types cycle through.
+
+		if currentSentence == 0:
+
+			nextSentence = selectSentence(descriptions)
+			currentSentence = 1
+			novel.append(descriptions[nextSentence])
+
+		if currentSentence == 1:
+			nextSentence = selectSentence(interactions)
+			currentSentence = 2
+			novel.append(interactions[nextSentence])
+
+		if currentSentence == 2:
+			nextSentence = selectSentence(afterthoughts)
+			currentSentence = 3
+			novel.append(afterthoughts[nextSentence])
+
+		if currentSentence == 3:
+			nextSentence = selectSentence(descriptions)
+			currentSentence = 1
+			novel.append(descriptions[nextSentence])
+
+		#adding the length of the last sentence to the total character count, determines whether the loop runs again
+		count += len(descriptions[nextSentence])
+
+	#put a final afterthought sentence to end the novel
+	nextSentence = selectSentence(afterthoughts)
+	novel.append(afterthoughts[nextSentence])
+
+	#writing to a text file
+	file = open(filename, "w")
+
+	#looping through novel list and writing each sentence to the file
+	for sentence in novel:
+		file.write(sentence + ". ")
+
+	file.close()
+
+	print count
+	print "Novel is generated at " + filename
+
 
 
 #connect to the database
